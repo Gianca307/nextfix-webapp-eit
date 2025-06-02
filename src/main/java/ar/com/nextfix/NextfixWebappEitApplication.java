eb08cec2fdc1bd81ff603dd09d60dc4759e8d6e5
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-06-02 00:07:14 -0300

    Guarda cambios en .gitignore antes de hacer pull

---
+++ src/main/java/ar/com/nextfix/NextfixWebappEitApplication.java
@@ -0,0 +1,13 @@
+package ar.com.nextfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextfixWebappEitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextfixWebappEitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/nextfix/NextfixWebappEitApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.nextfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextfixWebappEitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
