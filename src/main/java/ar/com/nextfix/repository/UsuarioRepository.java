24dd8d9e2d99f64fea4b72e2f859605045fefcbe
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-06-02 00:50:18 -0300

    Arregla usuario repository

--- src/main/java/ar/com/nextfix/repository/UsuarioRepository.java
+++ src/main/java/ar/com/nextfix/repository/UsuarioRepository.java
@@ -2,13 +2,14 @@ package ar.com.nextfix.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ar.com.nextfix.domain.Director;
 import ar.com.nextfix.domain.Usuario;
 
 @Repository
-public interface UsuarioRepository {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	Usuario findByUsername(String username);
     Usuario findByDirector(Director director);
     List<Usuario> findByDirectorIsNotNull();
